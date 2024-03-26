e91983b9ded3a972d16c2dd6f7f9206f27e4ea95
kasia-cz <czak.katarzyna1@gmail.com>
2024-03-26 22:19:43 +0100

    Create WebAPI project and add DbContext

---
+++ NewsPlatform.Data/Context/NewsPlatformDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsPlatform.Data.Entities;
+
+namespace NewsPlatform.Data.Context
+{
+    public class NewsPlatformDbContext : DbContext
+    {
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public NewsPlatformDbContext(DbContextOptions<NewsPlatformDbContext> options) : base(options) { }
+    }
+}
