35d198300bc563f7039c7a18d978f6898ca89fbd
kasia-cz <czak.katarzyna1@gmail.com>
2024-04-16 14:43:29 +0200

    Add Topic DTOs and mapping profile

---
+++ NewsPlatform.Application/DTOs/TopicDTOs/AddTopicDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace NewsPlatform.Application.DTOs.TopicDTOs
+{
+    public class AddTopicDTO
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ NewsPlatform.Application/MappingProfiles/TopicMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NewsPlatform.Application.DTOs.TopicDTOs;
+using NewsPlatform.Data.Entities;
+
+namespace NewsPlatform.Application.MappingProfiles
+{
+    public class TopicMappingProfile : Profile
+    {
+        public TopicMappingProfile()
+        {
+            CreateMap<AddTopicDTO, Topic>();
+            CreateMap<Topic, ReturnTopicDTO>();
+        }
+    }
+}
