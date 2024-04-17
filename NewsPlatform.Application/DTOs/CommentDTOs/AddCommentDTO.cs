6264a95d298297cc1bec01024c2ebf6063d1d748
kasia-cz <czak.katarzyna1@gmail.com>
2024-04-17 19:59:16 +0200

    Add Comment DTOs

---
+++ NewsPlatform.Application/DTOs/CommentDTOs/AddCommentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsPlatform.Application.DTOs.CommentDTOs
+{
+    public class AddCommentDTO
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid NewsId { get; set; }
+    }
+}
---
+++ NewsPlatform.Application/DTOs/CommentDTOs/ReturnCommentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsPlatform.Application.DTOs.CommentDTOs
+{
+    public class ReturnCommentDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishTime { get; set; }
+        public string UserName { get; set; }
+    }
+}
